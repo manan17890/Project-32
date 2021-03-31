511a8a0c1defd42b01fcf331cc40d283af9d86e4
Mananveer Singh <74530708+manan17890@users.noreply.github.com>
2021-03-31 12:29:43 +0530

    Add files via upload

---
+++ Project-32/PRO-C-32-Template-main/sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+   getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+    background(backgroundImg);
+    Engine.update(engine);
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var responseJSON = await response.json()
+    //console.log(responseJSON);
+    var dateTime = responseJSON.datetime;
+    var hour = dateTime.slice(11 , 13)
+    if(hour >= 04 && hour <= 06){
+        bg = "sunrise1.png";
+    }
+    else if(hour >= 06 && hour <= 08){
+        bg = "sunrise2.png"
+    }
+    else if(hour >= 08 && hour == 0){
+        bg = "sunset10.png"
+    }
+    else if(hour == 0 && hour <= 03){
+        bg = "sunset11.png"
+    }
+    else{
+        bg = "sunset12.png"
+    }
+    
+    backgroundImg = loadImage(bg);
+}
